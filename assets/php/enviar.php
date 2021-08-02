283722a5d11aaaf0bf8fdb98199882521707f03a
NicolasFigueredoo <86845893+NicolasFigueredoo@users.noreply.github.com>
2021-08-02 12:14:31 -0300

    add files

---
+++ assets/php/enviar.php
@@ -0,0 +1,21 @@
+<?php
+       //llamando a las variables
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
+   
+    //datos para el correo
+    $destinatario = "gtytptsp@gmail.com";
+    $asunto = "Contacto de nightdrive";
+
+    $carta = "De: $nombre . $apellido \n";
+    $carta .= "Correo: $correo \n";
+    $carta .= "Mensaje: $mensaje";
+
+    //enviando mensaje
+    mail($destinatario, $asunto, $carta);
+    header('location: index.html')
+
+
+?>
